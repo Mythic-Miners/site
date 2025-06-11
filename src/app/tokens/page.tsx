e735c6ea8d14b5818e8401aeb95d7a31f68e6c9e
Rafael Cerri <rafaelbcerri@gmail.com>
2025-06-11 00:57:56 -0300

    Cliff page

--- src/app/tokens/page.tsx
+++ src/app/tokens/page.tsx
@@ -10,6 +10,7 @@ import { useAuth } from '@/context/AuthContext';
 import { useLanguage } from '@/context/LanguageContext';
 
 interface Holdings {
+  cliffDate: Date;
   investment: {
     principal: number;
     bonus: number;
@@ -87,6 +88,55 @@ export default function TokensPage() {
           <div className="flex flex-col items-center justify-center h-full">
             <h3 className="max-w-3xl text-center text-2xl font-bold text-gray-300 mb-10 mt-4">
               {t('tokensPage.didNotParticipate')}
+            </h3>
+            <AuthButton />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  if (
+    isConnected &&
+    holdings &&
+    'data' in holdings &&
+    holdings.data.cliffDate
+  ) {
+    return (
+      <div className="min-h-screen bg-linear-to-b from-blue-950 to-gray-900 p-4">
+        <button
+          onClick={() => router.back()}
+          className="flex items-center text-gray-300 hover:text-cyan-400 transition-colors mb-8"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6 mr-2"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M10 19l-7-7m0 0l7-7m-7 7h18"
+            />
+          </svg>
+          {t('tokensPage.back')}
+        </button>
+
+        <div className="max-w-6xl mx-auto">
+          <h1 className="text-4xl font-bold text-cyan-500 mb-12 mythic-text-shadow">
+            {t('tokensPage.title')}
+          </h1>
+          <div className="flex flex-col items-center justify-center h-full">
+            <h3 className="max-w-3xl text-center text-2xl font-bold text-gray-300 mb-10 mt-4">
+              {t('tokensPage.cliff', {
+                date: new Intl.DateTimeFormat(language, {
+                  dateStyle: 'medium',
+                  timeStyle: 'short',
+                }).format(new Date(holdings?.data.cliffDate)),
+              })}
             </h3>
             <AuthButton />
           </div>
