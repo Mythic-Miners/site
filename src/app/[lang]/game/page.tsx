33272df7ae66ce0c1b1bf46b08107c29758256d7
Rafael Cerri <rafaelbcerri@gmail.com>
2025-07-30 21:59:58 -0300

    New build

--- src/app/[lang]/game/page.tsx
+++ src/app/[lang]/game/page.tsx
@@ -1,12 +1,16 @@
 'use client';
 
 // import { useEffect } from 'react';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+
+import Game from '@/components/ui/Game';
 
 // import useGameQuery from '@/api/game';
 
 export default function GamePage() {
   const { t } = useTranslation();
+  const [isRequestLoading, setIsRequestLoading] = useState(false);
   // const { data: game, isPending } = useGameQuery();
 
   // useEffect(() => {
@@ -30,20 +34,20 @@ export default function GamePage() {
   //   return () => clearTimeout(timeout);
   // }, []);
 
-  if (true) {
-    return (
-      <div className="max-w-7xl mx-auto mt-12 px-8">
-        <div className="my-20 flex justify-center items-center">
-          <div className="w-[800px] h-[500px] border-4 border-black rounded-[20px] bg-indigo-950 flex flex-col justify-center items-center text-center px-8">
-            <p className="text-2xl text-white mb-4">{t('game.maintenance')}</p>
-            <p className="text-lg text-gray-300">
-              {t('game.maintenanceDescription')}
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  // if (true) {
+  //   return (
+  //     <div className="max-w-7xl mx-auto mt-12 px-8">
+  //       <div className="my-20 flex justify-center items-center">
+  //         <div className="w-[800px] h-[500px] border-4 border-black rounded-[20px] bg-indigo-950 flex flex-col justify-center items-center text-center px-8">
+  //           <p className="text-2xl text-white mb-4">{t('game.maintenance')}</p>
+  //           <p className="text-lg text-gray-300">
+  //             {t('game.maintenanceDescription')}
+  //           </p>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
   // if (isPending) {
   //   return (
@@ -106,11 +110,35 @@ export default function GamePage() {
   // //   );
   // // }
 
-  // return (
-  //   <div className="max-w-7xl mx-auto mt-12 px-8">
-  //     <div className="my-20 flex justify-center items-center">
-  //       <Game />
-  //     </div>
-  //   </div>
-  // );
+  return (
+    <div className="max-w-7xl mx-auto mt-12 px-8">
+      <div className="relative my-20 flex justify-center items-center">
+        <Game />
+        {isRequestLoading && (
+          <div className="absolute top-0 left-0 h-full w-full rounded-[20px] bg-indigo-950/20 flex justify-center items-center backdrop-blur-sm z-10">
+            <svg
+              className="animate-spin h-8 w-8 text-cyan-50"
+              xmlns="http:www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="m4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
--- src/components/ui/Game.tsx
+++ src/components/ui/Game.tsx
@@ -5,6 +5,7 @@ import { Unity, useUnityContext } from 'react-unity-webgl';
 
 export default function Game() {
   const [isDataLoaded, setIsDataLoaded] = useState(false);
+  const [isRequestLoading, setIsRequestLoading] = useState(false);
   const { addEventListener, removeEventListener, unityProvider, isLoaded } =
     useUnityContext({
       loaderUrl: '/assets/game/mythicminers.loader.js',
@@ -15,6 +16,7 @@ export default function Game() {
 
   useEffect(() => {
     const handleDataLoadedEvent = () => {
+      console.log('data loaded');
       setIsDataLoaded(true);
     };
 
@@ -22,11 +24,31 @@ export default function Game() {
       window.location.reload();
     };
 
+    const handleRequestLoadingTrueEvent = () => {
+      console.log('request loading true');
+      setIsRequestLoading(true);
+    };
+
+    const handleRequestLoadingFalseEvent = () => {
+      console.log('request loading false');
+      setIsRequestLoading(false);
+    };
+
     addEventListener('data_loaded', handleDataLoadedEvent);
     addEventListener('respawn', handleRespawnEvent);
+    addEventListener('request_loading_true', handleRequestLoadingTrueEvent);
+    addEventListener('request_loading_false', handleRequestLoadingFalseEvent);
     return () => {
       removeEventListener('data_loaded', handleDataLoadedEvent);
       removeEventListener('respawn', handleRespawnEvent);
+      removeEventListener(
+        'request_loading_true',
+        handleRequestLoadingTrueEvent,
+      );
+      removeEventListener(
+        'request_loading_false',
+        handleRequestLoadingFalseEvent,
+      );
     };
   }, [addEventListener, removeEventListener]);
 
@@ -43,6 +65,30 @@ export default function Game() {
       />
       {(!isDataLoaded || !isLoaded) && (
         <div className="absolute top-0 left-0 w-[800px] h-[500px]  rounded-[20px] bg-zinc-950 flex justify-center items-center">
+          <svg
+            className="animate-spin h-8 w-8 text-cyan-50"
+            xmlns="http:ww.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="4"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="m4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
+      )}
+      {isRequestLoading && (
+        <div className="absolute top-0 left-0 w-[800px] h-[500px]  rounded-[20px] bg-zinc-950/20 flex justify-center items-center backdrop-blur-xs z-10">
           <svg
             className="animate-spin h-8 w-8 text-cyan-50"
             xmlns="http://www.w3.org/2000/svg"
