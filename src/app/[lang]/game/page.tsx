a4a2471aae8fa3168af5e2c3594360de58f6694f
Rafael Cerri <rafaelbcerri@gmail.com>
2025-09-05 17:06:59 -0300

    remove maintenance

--- src/app/[lang]/game/page.tsx
+++ src/app/[lang]/game/page.tsx
@@ -31,20 +31,20 @@ export default function GamePage() {
     return () => clearTimeout(timeout);
   }, []);
 
-  if (true) {
-    return (
-      <div className="max-w-7xl mx-auto mt-12 px-8">
-        <div className="my-20 flex justify-center items-center">
-          <div className="w-[800px] h-[500px] border-4 border-black rounded-[20px] bg-indigo-950 flex flex-col justify-center items-center text-center px-8">
-            <p className="text-2xl text-white mb-4">{t('game.maintenance')}</p>
-            <p className="text-lg text-gray-300">
-              {t('game.maintenanceDescription')}
-            </p>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  // if (true) {
+  //   return (
+  //     <div className="max-w-7xl mx-auto mt-12 px-8">
+  //       <div className="my-20 flex justify-center items-center">
+  //         <div className="w-[800px] h-[500px] border-4 border-black rounded-[20px] bg-indigo-950 flex flex-col justify-center items-center text-center px-8">
+  //           <p className="text-2xl text-white mb-4">{t('game.maintenance')}</p>
+  //           <p className="text-lg text-gray-300">
+  //             {t('game.maintenanceDescription')}
+  //           </p>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
   if (isPending) {
     return (
